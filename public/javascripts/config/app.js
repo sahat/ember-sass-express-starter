8d2029dec69989d6776ae952436774fe8ff3a884
Sahat Yalkabov <sakhat@gmail.com>
2014-01-11 04:08:35 -0500

    MongoDB serialization for RESTSerializer

--- public/javascripts/config/app.js
+++ public/javascripts/config/app.js
@@ -5,6 +5,10 @@ require('../vendor/ember-data');
 
 var App = Ember.Application.create();
 
+App.ApplicationSerializer = DS.RESTSerializer.extend({
+  primaryKey: '_id'
+});
+
 App.ApplicationAdapter = DS.RESTAdapter.extend({
   namespace: 'api/v1'
 });
