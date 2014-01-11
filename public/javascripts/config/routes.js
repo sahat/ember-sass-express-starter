20108530b7b7310d6706e2e2e204d5f396d79820
Sahat Yalkabov <sakhat@gmail.com>
2014-01-11 05:32:02 -0500

    App routes

---
+++ public/javascripts/config/routes.js
@@ -0,0 +1,8 @@
+var App = require('./app');
+
+App.Router.map(function() {
+  this.resource('people');
+  this.resource('person', { path: '/people/:person_id' });
+  this.route('edit_person', { path: '/people/:person_id/edit' });
+  this.route('new_person', { path: '/people/new' });
+});
