5b95ffc4a670c7453be49f08b2f28e6904d75053
Sahat Yalkabov <sakhat@gmail.com>
2014-01-11 04:56:20 -0500

    Placed save() method inside actions of EditPersonController

--- public/javascripts/controllers/edit_person_controller.js
+++ public/javascripts/controllers/edit_person_controller.js
@@ -1,16 +1,15 @@
 var EditPersonController = Ember.ObjectController.extend({
-
-  save: function() {
-    this.get('model').save();
-    this.redirectToModel();
+  actions: {
+    save: function() {
+      this.get('model').save();
+      this.redirectToModel();
+    }
   },
-
   redirectToModel: function() {
     var router = this.get('target');
     var model = this.get('model');
     router.transitionTo('person', model);
   }
-
 });
 
 module.exports = EditPersonController;
