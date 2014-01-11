868f0e249d94b18e48d374abc87a43743d9807af
Sahat Yalkabov <sakhat@gmail.com>
2014-01-11 07:48:51 -0500

    JSDoc comments on API endpoints

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
+/**
+ * Module dependencies.
+ */
+
 var express = require('express');
 var mongoose = require('mongoose');
 var path = require('path');
 var sass = require('node-sass');
 
 mongoose.connect('localhost');
+
 var personSchema = new mongoose.Schema({
   name:  String,
   age: Number
 });
+
 var Person = mongoose.model('Person', personSchema);
 
 var app = express();
@@ -28,28 +34,52 @@ app.use(sass.middleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// Find
+
+/**
+ * Find person by id.
+ *
+ * @param {string} id
+ * @returns {object} person
+ */
+
 app.get('/api/v1/people/:id', function(req, res) {
   Person.findById(req.params.id, function(err, person) {
     res.send({ person: person });
   });
 });
 
-// Find All
+/**
+ * Find all people.
+ *
+ * @returns {object} person
+ */
+
 app.get('/api/v1/people', function(req, res) {
   Person.find(function(err, people) {
     res.send({ person: people });
   });
 });
 
-// Update
+/**
+ * Update person by id.
+ *
+ * @param {string} id
+ * @returns {object} person
+ */
+
 app.put('/api/v1/people/:id', function(req, res) {
   Person.findByIdAndUpdate(req.params.id, req.body.person, function(err, person) {
     res.send({ person: person });
   });
 });
 
-// Create
+/**
+ * Create new person.
+ *
+ * @param {object} person
+ * @returns {object} person
+ */
+
 app.post('/api/v1/people', function(req, res) {
   var person = new Person(req.body.person);
   person.save(function(err) {
@@ -57,13 +87,18 @@ app.post('/api/v1/people', function(req, res) {
   });
 });
 
-// Delete
+/**
+ * Delete person by id.
+ *
+ * @param {string} id
+ * @returns 200 OK
+ */
+
 app.del('/api/v1/people/:id', function(req, res) {
   Person.findById(req.params.id).remove(function(err) {
     res.send(200);
   });
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(app.get('port'));
+console.log('Express server running on port ' + app.get('port'));
