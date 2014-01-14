cac400377d47213f794a264ee21ca38933b00557
Sahat Yalkabov <sakhat@gmail.com>
2014-01-14 07:43:39 -0500

    Login and sign up routes, controllers, templates.

--- public/javascripts/config/routes.js
+++ public/javascripts/config/routes.js
@@ -1,6 +1,8 @@
 var App = require('./app');
 
 App.Router.map(function() {
+  this.resource('login');
+  this.resource('signup');
   this.resource('people');
   this.resource('person', { path: '/people/:person_id' });
   this.route('edit_person', { path: '/people/:person_id/edit' });
