36a69067fc40ba49cebf9eed29caba3786a5833d
Sahat Yalkabov <sakhat@gmail.com>
2014-01-14 23:00:49 -0500

    Transition to home after login or signup button is clicked

--- public/javascripts/controllers/login_controller.js
+++ public/javascripts/controllers/login_controller.js
@@ -1,5 +1,10 @@
 var LoginController = Ember.ObjectController.extend({
-
+  actions: {
+    login: function() {
+      console.log('login');
+      this.transitionToRoute('index');
+    }
+  }
 });
 
 module.exports = LoginController;
--- public/javascripts/controllers/signup_controller.js
+++ public/javascripts/controllers/signup_controller.js
@@ -1,5 +1,10 @@
 var SignupController = Ember.ObjectController.extend({
-
+  actions: {
+    signup: function() {
+      console.log('signup');
+      this.transitionToRoute('index');
+    }
+  }
 });
 
 module.exports = SignupController;
