d52c1d14a77b75517f546ef5114aeebe441f0caa
Sahat Yalkabov <sakhat@gmail.com>
2014-01-17 17:30:43 -0500

    Created a login action on LoginController for processing a sign in request

--- public/javascripts/controllers/login_controller.js
+++ public/javascripts/controllers/login_controller.js
@@ -1,8 +1,22 @@
 var LoginController = Ember.ObjectController.extend({
+  loginFailed: false,
+  isProcessing: false,
+
   actions: {
     login: function() {
-      alert('Fake Login');
-      this.transitionToRoute('index');
+      this.setProperties({
+        loginFailed: false,
+        isProcessing: true
+      });
+      $.post('/login', {
+        username: this.get('username'),
+        password: this.get('password')
+      }).done(function() {
+        console.log('Login success');
+      }).fail(function() {
+        this.set('isProcessing', false);
+        this.set('loginFailed', true);
+      }.bind(this));
     }
   }
 });
