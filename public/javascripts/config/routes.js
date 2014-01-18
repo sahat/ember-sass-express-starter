b46403ef9bcc2c97843f64b69f406abefda219b1
Sahat Yalkabov <sakhat@gmail.com>
2014-01-18 00:18:34 -0500

    Login, signup, and people are now routes instead of resources

--- public/javascripts/config/routes.js
+++ public/javascripts/config/routes.js
@@ -1,9 +1,9 @@
 var App = require('./app');
 
 App.Router.map(function() {
-  this.resource('login');
-  this.resource('signup');
-  this.resource('people');
+  this.route('login');
+  this.route('signup');
+  this.route('people');
   this.resource('person', { path: '/people/:person_id' });
   this.route('edit_person', { path: '/people/:person_id/edit' });
   this.route('new_person', { path: '/people/new' });
