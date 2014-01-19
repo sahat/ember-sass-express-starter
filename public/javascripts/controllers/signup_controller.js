9afdcfeb4ce823367f3f0d9197bb4d1f61075038
Sahat Yalkabov <sakhat@gmail.com>
2014-01-18 20:46:34 -0500

    Updated signup controller

--- public/javascripts/controllers/signup_controller.js
+++ public/javascripts/controllers/signup_controller.js
@@ -1,8 +1,15 @@
-var SignupController = Ember.ObjectController.extend({
+var SignupController = Ember.Controller.extend({
   actions: {
     signup: function() {
-      alert('Fake Signup');
-      this.transitionToRoute('index');
+      $.post('/signup', {
+        username: this.get('username'),
+        email: this.get('email'),
+        password: this.get('password')
+      }).done(function(data) {
+          this.transitionToRoute('login');
+      }).fail(function(error) {
+          console.log(error);
+      }.bind(this));
     }
   }
 });
