87bc64c1dd7b6c037243eb7938461c958c223aea
Sahat Yalkabov <sakhat@gmail.com>
2014-01-19 18:05:23 -0500

    Error message "incorrect username or password" on failed login.

--- public/javascripts/controllers/login_controller.js
+++ public/javascripts/controllers/login_controller.js
@@ -1,3 +1,9 @@
-var LoginController = Ember.Controller.extend(Ember.SimpleAuth.LoginControllerMixin);
+var LoginController = Ember.Controller.extend(Ember.SimpleAuth.LoginControllerMixin, {
+  actions: {
+    loginFailed: function(xhr) {
+      this.set('errorMessage', xhr.responseText);
+    }
+  }
+});
 
 module.exports = LoginController;
