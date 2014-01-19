38c18b50390de6b6c0118c25cfa28d016f665b6f
Sahat Yalkabov <sakhat@gmail.com>
2014-01-18 20:36:19 -0500

    Integrated ember-simple-auth with login controller

--- public/javascripts/controllers/login_controller.js
+++ public/javascripts/controllers/login_controller.js
@@ -1,31 +1,3 @@
-var LoginController = Ember.Controller.extend({
-  loginFailed: false,
-  isProcessing: false,
-
-  actions: {
-    login: function() {
-      this.auth.signIn({
-        data: this.getProperties('username', 'password')
-      });
-
-//  this.setProperties({
-//        loginFailed: false,
-//        isProcessing: true
-//      });
-//      $.post('/login', {
-//        username: this.get('username'),
-//        password: this.get('password')
-//      }).done(function(data) {
-//          console.log(data);
-//          //this.reset();
-//          this.set('token', data.token);
-//          this.transitionToRoute('index');
-//      }).fail(function(error) {
-//        this.set('isProcessing', false);
-//        this.set('loginFailed', true);
-//      }.bind(this));
-    }
-  }
-});
+var LoginController = Ember.Controller.extend(Ember.SimpleAuth.LoginControllerMixin);
 
 module.exports = LoginController;
