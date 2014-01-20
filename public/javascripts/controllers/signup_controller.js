46e60a6b3bf0f6c4d4e3b4d16736893b1e159e7e
Sahat Yalkabov <sakhat@gmail.com>
2014-01-20 02:28:14 -0500

    Refactored signup controller.
    Using $.ajax now with context: this, to properly transition to login page on success.

--- public/javascripts/controllers/signup_controller.js
+++ public/javascripts/controllers/signup_controller.js
@@ -1,15 +1,16 @@
 var SignupController = Ember.Controller.extend({
   actions: {
     signup: function() {
-      $.post('/signup', {
-        username: this.get('username'),
-        email: this.get('email'),
-        password: this.get('password')
-      }).done(function(data) {
-          this.transitionToRoute('login');
-      }).fail(function(error) {
-          console.log(error);
-      }.bind(this));
+      $.ajax({
+        type: 'POST',
+        url: '/signup',
+        context: this,
+        data: this.getProperties('username', 'email', 'password', 'confirmPassword')
+      }).done(function() {
+        this.transitionToRoute('login');
+      }).fail(function(xhr) {
+        this.set('errorMessage', xhr.responseText);
+      });
     }
   }
 });
