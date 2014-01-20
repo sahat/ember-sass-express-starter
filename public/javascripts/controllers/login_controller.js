14725baa6dc955dc2f487d6c6ee54b758b6e6796
Sahat Yalkabov <sakhat@gmail.com>
2014-01-20 02:07:59 -0500

    Removed properties and methods in login controller and view that are no longer used

--- public/javascripts/controllers/login_controller.js
+++ public/javascripts/controllers/login_controller.js
@@ -1,12 +1,7 @@
 var LoginController = Ember.Controller.extend(Ember.SimpleAuth.LoginControllerMixin, {
-  error: false,
   actions: {
     loginFailed: function(xhr) {
-      this.set('error', true);
       this.set('errorMessage', xhr.responseText);
-    },
-    error: function(value) {
-      this.set('error', value);
     }
   }
 });
--- public/javascripts/views/login_view.js
+++ public/javascripts/views/login_view.js
@@ -1,11 +1,5 @@
 var LoginView = Ember.View.extend({
-  didInsertElement: function() {
-    if (this.get('controller.error')) {
-      this.$('#password').focus();
-    }
-  },
   willDestroyElement: function() {
-    this.get('controller').send('error', false);
     this.get('context').set('errorMessage', null);
   }
 });
